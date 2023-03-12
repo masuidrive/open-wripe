fdd49c911d569e50cb03b095bf95e9acb3348bd3
Yasumasa Ashida <ys.ashida@gmail.com>
2023-03-12 17:47:43 +0900

    [wip] update rails to 5.2.8.1 (modify spec)

--- spec/features/create_new_page_spec.rb
+++ spec/features/create_new_page_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 feature 'create new note', :js => true do
+  before(:each) do
+    @user = FactoryBot.create(:testdrive1)
+  end
+
   scenario 'create note' do
-    user = FactoryBot.create(:testdrive1)
     test_login 'testdrive1'
 
     find('#navigator-new').click
@@ -16,10 +19,10 @@ feature 'create new note', :js => true do
     sleep 0.5
     wait_until_visible "#{$el[:edit_save]} span[name='save']"
 
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 0
-    expect(user.pages.first.title).to eq "TEST NOTE"
-    expect(user.pages.first.body).to eq "TEST\n123\n"
+    expect(@user.pages.count).to eq 1
+    expect(@user.pages.first.lock_version).to eq 0
+    expect(@user.pages.first.title).to eq "TEST NOTE"
+    expect(@user.pages.first.body).to eq "TEST\n123\n"
 
     # modify
     find($el[:edit_title]).set "TEST NOTE2"
@@ -29,9 +32,9 @@ feature 'create new note', :js => true do
     sleep 0.5
     wait_until_visible "#{$el[:edit_save]} span[name='save']"
 
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 1
-    expect(user.pages.first.title).to eq "TEST NOTE2"
-    expect(user.pages.first.body).to eq "TEST\nABC\n"
+    expect(@user.pages.count).to eq 1
+    expect(@user.pages.first.lock_version).to eq 1
+    expect(@user.pages.first.title).to eq "TEST NOTE2"
+    expect(@user.pages.first.body).to eq "TEST\nABC\n"
   end
 end
--- spec/features/modify_page_spec.rb
+++ spec/features/modify_page_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 feature 'Modify note', :js => true do
-  scenario 'load exists note, modify and save' do
-    user = FactoryBot.create(:testdrive1)
-    page = user.pages.create :title => 'TITLE', :body => 'BODY'
 
+  before(:each) do
+    @user = FactoryBot.create(:testdrive1)
+    @page = FactoryBot.create(:testpage, user: @user, title: 'TITLE', body: 'BODY')
+  end
+
+  scenario 'load exists note, modify and save' do
     test_login 'testdrive1'
 
     wait_until do
       has_css?('#list-page', visible: true) && find('#list-page')&.text.present?
     end
 
-    expect(find("#list-page-#{page.key} .title")).to have_content("TITLE")
-    find("#list-page-#{page.key} .title a").click
+    expect(find("#list-page-#{@page.key} .title")).to have_content("TITLE")
+    find("#list-page-#{@page.key} .title a").click
     wait_until_visible $el[:edit_body]
 
     find($el[:edit_title]).set "TEST NOTE"
@@ -20,99 +23,93 @@ feature 'Modify note', :js => true do
 
     wait_until_visible "#{$el[:edit_save]} span[name='save']"
 
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 1
-    expect(user.pages.first.title).to eq "TEST NOTE"
-    expect(user.pages.first.body).to eq "TEST\n123\n"
+    expect(@user.pages.count).to eq 1
+    expect(@user.pages.first.lock_version).to eq 1
+    expect(@user.pages.first.title).to eq "TEST NOTE"
+    expect(@user.pages.first.body).to eq "TEST\n123\n"
 
     find('#navigator-index').click
     wait_until_visible '#list-page'
 
-    expect(wait_and_find("#list-page-#{page.key} .title")).to have_content("TEST NOTE")
+    expect(wait_and_find("#list-page-#{@page.key} .title")).to have_content("TEST NOTE")
   end
-end
 
-feature 'Modify note and conflict', :js => true do
-  scenario 'modify and conflict' do
-    user = FactoryBot.create(:testdrive1)
-    page = user.pages.create :title => 'TITLE', :body => 'BODY'
+  feature ' and conflict', :js => true do
+    scenario 'modify and conflict' do
+      test_login 'testdrive1'
 
-    test_login 'testdrive1'
+      wait_until do
+        has_css?('#list-page', visible: true) && find('#list-page')&.text.present?
+      end
 
-    wait_until do
-      has_css?('#list-page', visible: true) && find('#list-page')&.text.present?
+      expect(find("#list-page-#{@page.key} .title")).to have_content("TITLE")
+      find("#list-page-#{@page.key} .title a").click
+
+      wait_until_visible $el[:edit_body]
+      expect(find($el[:edit_body]).value).to eq 'BODY'
+
+      @page.update_attributes body: "BODY\n1\n2\n"
+
+      find($el[:edit_body]).set "BODY\n123\n"
+      find($el[:edit_save]).click
+
+      wait_until_visible "#{$el[:edit_save]} span[name='save']"
+
+      wait_until_visible '#page-edit-conflict .btn-close'
+      sleep 1.0 # wait fade out
+      find('#page-edit-conflict .btn-close').click
+      sleep 1.0 # wait fade out
+
+      expect(find($el[:edit_body]).value).to eq "BODY\n123\n\n1\n2\n\n"
+      find($el[:edit_save]).click
+
+      wait_until_visible "#{$el[:edit_save]} span[name='save']"
+
+      expect(@user.pages.count).to eq 1
+      expect(@user.pages.first.lock_version).to eq 2
+      expect(@user.pages.first.body).to eq "BODY\n123\n\n1\n2\n\n"
     end
-
-    expect(find("#list-page-#{page.key} .title")).to have_content("TITLE")
-    find("#list-page-#{page.key} .title a").click
-
-    wait_until_visible $el[:edit_body]
-    expect(find($el[:edit_body]).value).to eq 'BODY'
-
-    page.update_attributes body: "BODY\n1\n2\n"
-
-    find($el[:edit_body]).set "BODY\n123\n"
-    find($el[:edit_save]).click
-
-    wait_until_visible "#{$el[:edit_save]} span[name='save']"
-
-    wait_until_visible '#page-edit-conflict .btn-close'
-    sleep 1.0 # wait fade out
-    find('#page-edit-conflict .btn-close').click
-    sleep 1.0 # wait fade out
-
-    expect(find($el[:edit_body]).value).to eq "BODY\n123\n\n1\n2\n\n"
-    find($el[:edit_save]).click
-
-    wait_until_visible "#{$el[:edit_save]} span[name='save']"
-
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 2
-    expect(user.pages.first.body).to eq "BODY\n123\n\n1\n2\n\n"
   end
-end
 
-feature 'Modify note and delay', :js => true do
-  scenario 'modify and conflict' do
-    user = FactoryBot.create(:testdrive1)
-    page = user.pages.create :title => 'TITLE', :body => 'BODY'
+  feature ' and delay', :js => true do
+    scenario 'modify and conflict' do
+      test_login 'testdrive1'
 
-    test_login 'testdrive1'
+      wait_until do
+        has_css?('#list-page', visible: true) && find('#list-page')&.text.present?
+      end
 
-    wait_until do
-      has_css?('#list-page', visible: true) && find('#list-page')&.text.present?
+      expect(find("#list-page-#{@page.key} .title")).to have_content("TITLE")
+      find("#list-page-#{@page.key} .title a").click
+
+      wait_until_visible $el[:edit_body]
+      expect(find($el[:edit_body]).value).to eq 'BODY'
+
+      find($el[:edit_body]).set "BODY\n123\n"
+
+      PagesController._delay = 2.0
+      find($el[:edit_save]).click
+      sleep 0.5
+
+      expect(find($el[:edit_body]).value).to eq "BODY\n123\n"
+      find($el[:edit_body]).set "BODY\n1\n2"
+
+      wait_until_visible "#{$el[:edit_save]} span[name='save']"
+      expect(find($el[:edit_body]).value).to eq "BODY\n1\n2"
+
+      expect(@user.pages.count).to eq 1
+      expect(@user.pages.first.lock_version).to eq 1
+      expect(@user.pages.first.body).to eq "BODY\n123\n"
+
+      PagesController._delay = nil
+      find($el[:edit_save]).click
+
+      wait_until_visible "#{$el[:edit_save]} span[name='save']"
+      expect(find($el[:edit_body]).value).to eq "BODY\n1\n2"
+
+      expect(@user.pages.count).to eq 1
+      expect(@user.pages.first.lock_version).to eq 2
+      expect(@user.pages.first.body).to eq "BODY\n1\n2"
     end
-
-    expect(find("#list-page-#{page.key} .title")).to have_content("TITLE")
-    find("#list-page-#{page.key} .title a").click
-
-    wait_until_visible $el[:edit_body]
-    expect(find($el[:edit_body]).value).to eq 'BODY'
-
-    find($el[:edit_body]).set "BODY\n123\n"
-
-    PagesController._delay = 2.0
-    find($el[:edit_save]).click
-    sleep 0.5
-
-    expect(find($el[:edit_body]).value).to eq "BODY\n123\n"
-    find($el[:edit_body]).set "BODY\n1\n2"
-
-    wait_until_visible "#{$el[:edit_save]} span[name='save']"
-    expect(find($el[:edit_body]).value).to eq "BODY\n1\n2"
-
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 1
-    expect(user.pages.first.body).to eq "BODY\n123\n"
-
-    PagesController._delay = nil
-    find($el[:edit_save]).click
-
-    wait_until_visible "#{$el[:edit_save]} span[name='save']"
-    expect(find($el[:edit_body]).value).to eq "BODY\n1\n2"
-
-    expect(user.pages.count).to eq 1
-    expect(user.pages.first.lock_version).to eq 2
-    expect(user.pages.first.body).to eq "BODY\n1\n2"
   end
 end
